c6ca5d6f57cc86484ca2b3896d10eb5df2b9be60
yar <muss.dosso@gmail.com>
2023-02-10 18:35:25 +0000

    Envoyer le projet sur repo distant

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MonCourriel.Models;
+using System.Diagnostics;
+
+namespace MonCourriel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Main()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MonCourriel.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string LastName { get; set; }
+        public string FisrtName { get; set; }
+        public string UserName { get; set; }
+        public string Tel { get; set; }
+    }
+}
